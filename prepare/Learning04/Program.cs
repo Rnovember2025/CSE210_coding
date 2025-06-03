85f7f84606dc31cf7b8f74071964458f47022213
Rnovember2025 <RCN16@protonmail.com>
2025-06-03 08:43:22 -0600

    Week 7 Prepare Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math = new MathAssignment("Ronan Nash", "Directional Derivitives", "Section 4.6", "Problems 1-100");
+        WritingAssignment paper = new WritingAssignment("Ronan Nash", "Research Paper", "AI's influence on Electrical Engineering");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        Console.WriteLine(paper.GetSummary());
+        Console.WriteLine(paper.GetWritingInformation());
     }
 }
